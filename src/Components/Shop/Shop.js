a8cca1a0eef9db201350325aaa65e9a7e3e73318
shanto360 <shanto.360@yahoo.com>
2022-04-01 17:31:20 +0600

    add-btn

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -3,23 +3,30 @@ import './shop.css';
 import Product from '../Product/Product';
 const Shop = () => {
     const[products, setProducts] = useState([])
+    const [cart, setCart] = useState([])
     useEffect( () =>{
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
     }, [])
+    const handleAddToClick = (product) =>{
+        const newCart = [...cart, product]
+        setCart(newCart)
+    }
     return (
         <div className='shop-container'>
             <div className="products-container">
                 {
                     products.map(product =><Product 
                         key={product.id}
-                        product = {product}>
+                        product = {product}
+                        handleAddToClick={handleAddToClick}>
                         </Product>)
                 }
             </div>
             <div className="cart-container">
                 <h4>Order Summary</h4>
+                <p>Selected Items: {cart.length}</p>
             </div>
         </div>
     );
