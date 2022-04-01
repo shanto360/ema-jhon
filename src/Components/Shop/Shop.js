506e16bb22688728c993dda2d0534f940a5f74a1
shanto360 <shanto.360@yahoo.com>
2022-04-01 14:08:11 +0600

    add-product-img-name

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import './shop.css'
+import React, { useEffect, useState } from 'react';
+import './shop.css';
+import Product from '../Product/Product';
 const Shop = () => {
+    const[products, setProducts] = useState([])
+    useEffect( () =>{
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    }, [])
     return (
         <div className='shop-container'>
             <div className="products-container">
-                <h3>This is products</h3>
+                {
+                    products.map(product =><Product 
+                        key={product.id}
+                        product = {product}>
+                        </Product>)
+                }
             </div>
             <div className="cart-container">
                 <h4>Order Summary</h4>
