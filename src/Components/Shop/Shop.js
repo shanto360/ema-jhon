05f0e8edc6fa1d1a283b925ed3d2fabb8d573c21
shanto360 <shanto.360@yahoo.com>
2022-04-01 13:10:21 +0600

    first commit

---
+++ src/Components/Shop/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './shop.css'
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                <h3>This is products</h3>
+            </div>
+            <div className="cart-container">
+                <h4>Order Summary</h4>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
